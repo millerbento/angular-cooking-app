6f458871c5b990b0cfd4b79f918e08e63207fc43
Miller Bento <millermbento@gmail.com>
2023-04-27 09:10:18 +1200

    Editing Recipes using the reactive forms approach
    - Created HTML form
    - Linked it using ReactiveFormsModule
    - Created basic logic to edit Recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,10 +11,12 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
   constructor(
-    private route: ActivatedRoute
-    ) {}
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+    ) { }
 
     ngOnInit() {
       this.route.params.
@@ -22,8 +26,34 @@ export class RecipeEditComponent implements OnInit {
           (params: Params) => {
             this.id = +params['id'];
             this.editMode = params['id'] != undefined;
+            this.initForm();
             console.log(this.editMode);
           }
         );
+    }
+
+    onSubmit() {
+      console.log(this.recipeForm);
+    }
+
+    //Using reactive forms approach here
+    private initForm() {
+      let recipeName = '';
+      let recipeImagePath = '';
+      let recipeDescription = '';
+
+
+      if (this.editMode) {
+        const recipe = this.recipeService.getRecipe(this.id);
+        recipeName = recipe.name;
+        recipeImagePath = recipe.imagePath;
+        recipeDescription = recipe.description;
+      }
+
+      this.recipeForm = new FormGroup({
+        'name': new FormControl(recipeName),
+        'imagePath': new FormControl(recipeImagePath),
+        'description': new FormControl(recipeDescription)
+      });
     }
 }
