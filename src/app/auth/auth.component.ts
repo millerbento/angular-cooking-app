2118fdea68e9d18acee892229a305c237c2e4ee2
Miller Bento <millermbento@gmail.com>
2023-05-19 11:05:35 +1200

    Start implementing authentication

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+
+export class AuthComponent {
+    isLoginMode = true;
+
+    onSwitchMode(){
+        this.isLoginMode = !this.isLoginMode;
+    }
+
+    onSubmit(form: NgForm){
+        if(!form.valid){
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        if(this.isLoginMode){
+
+        }else{
+
+        }
+        console.log(form.value);
+        form.reset();
+    }
+}
